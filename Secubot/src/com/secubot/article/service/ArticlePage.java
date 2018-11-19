5bd459512cb9f541e1569d13547698f111ca7bed
SEUNGHO <seung7642@gmail.com>
2018-11-19 16:45:11 +0900

    Working Complaints Page

--- Secubot/src/com/secubot/article/service/ArticlePage.java
+++ Secubot/src/com/secubot/article/service/ArticlePage.java
@@ -8,13 +8,15 @@ public class ArticlePage {
 
 	private int total;
 	private int currentPage;
+	private List<Article> content;
 	private int totalPages;
 	private int startPage;
 	private int endPage;
 
-	public ArticlePage(int total, int currentPage, int size) {
+	public ArticlePage(int total, int currentPage, int size, List<Article> content) {
 		this.total = total;
 		this.currentPage = currentPage;
+		this.content = content;
 		if (total == 0) {
 			totalPages = 0;
 			startPage = 0;
