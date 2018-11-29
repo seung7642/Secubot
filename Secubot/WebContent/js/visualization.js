900a9adec97c13eb35546eff0fd36c7c11f1a0ca
SEUNGHO <seung7642@gmail.com>
2018-11-30 02:30:49 +0900

    #zoom of d3.js

--- Secubot/WebContent/js/visualization.js
+++ Secubot/WebContent/js/visualization.js
@@ -1,43 +1,38 @@
 function visualization(data) {
 	d3.select("svg").selectAll("*").remove();
-	var formatAxis = d3.format("label");
 	
 	var xScale = d3.scale.linear().domain([data.minX, data.maxX]).range([20, 200]);
 	var yScale = d3.scale.linear().domain([data.minY, data.maxY]).range([480, 0]);
 	
-	/*
-	 *	설명: 차트의 축을 생성한다.
-	 */
-	var xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(1100).tickFormat(formatAxis);
-	// d3.select("#visualization").select("svg").append("g").attr("id", "xAxisG").call(xAxis);
+	var xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(1100);
 	var yAxis = d3.svg.axis().scale(yScale).orient("right").ticks(10).tickSize(1050);
-	// d3.select("#visualization").select("svg").append("g").attr("id", "yAxisG").call(yAxis);
 	
-	/*
-	 *	설명: 확대 & 드래그 기능을 추가한다.
-	 */
-	var zoom = zoomFunc(1, 10);
+	var zoom = 
+		d3.behavior.zoom()
+			.x(xScale)
+			.y(yScale)
+			.on("zoom", function() {
+				svg
+				.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+			});
 	
-	/*
-	 *	설명: Circle을 생성한다.
-	 *	개발 참고사항: data() 메서드의 매개변수 타입은 배열이다.
-	 */
-	var allG = d3.select("#visualization").select("svg").append("g").attr("class", "allG").call(zoom);
-	allG.selectAll("circle.behavior").data(data.ProcessList).enter().append("circle")
-	.attr("class", "behavior")
-	.attr("r", 5)
-	//.attr("cx", function(d) { return 1300 - scaleNum(0,data.maxX,10,600,d.x); })
-	.attr("cx", function(d) { return 200 - xScale(d.x); })
-	.attr("cy", function(d) { return yScale(d.y); })
-	.style("fill", "black");
+	var svg = 
+		d3.select("#visualization").select("svg").call(zoom)
+		.append("g")
+			.attr("class", "behaviorG");
 	
-	/*
-	 *	설명: 각 Circle에 이벤트를 추가한다.
-	 */
-	var evt = d3.select("g.allG").selectAll("circle");
-	evt.on("click", evtClick);
-	evt.on("mouseover", evtOver);
-	evt.on("mouseout", evtOut);
+	svg.selectAll("circle.behavior").data(data.ProcessList).enter()
+	.append("circle")
+		.attr("class", "behavior")
+		.attr("r", 5)
+		.attr("cx", function(d) { return 200 - xScale(d.x); })
+		.attr("cy", function(d) { return yScale(d.y); })
+		.style("fill", "black");
+	
+	d3.select("g.behaviorG").selectAll("circle")
+		.on("click", evtClick)
+		.on("mouseover", evtOver)
+		.on("mouseout", evtOut);
 	
 	function evtClick(d) {
 		// TODO: circle를 클릭했을 때 popup이 뜨는 로직
@@ -56,20 +51,6 @@ function visualization(data) {
 		// TODO: mouseout 이벤트 로직
 		d3.select(this).transition().duration(500).attr("r", 5);
 	}
-}
-
-function zoomFunc(x, y) {
-	var zoom = d3.behavior.zoom()
-	.scaleExtent([x, y])
-	.on("zoom", function() {
-		var e = d3.event;
-		var tx = Math.min(0, Math.max(e.translate[0], width - width * e.scale));
-		var ty = Math.min(0, Math.max(e.translate[1], height - height * e.scale));
-		
-		zoom.translate([tx, ty]);
-	});
-	
-	return zoom;
 }
 
 function dragFunc() {
@@ -98,7 +79,7 @@ function dragFunc() {
 function lineChart() {
 	var margin = { top: 30, right: 20, bottom: 30, left: 50 },
 		width = 600 - (margin.left + margin.right),
-		height = 270 - (margin.top + margin.bottom);
+		height = 500 - (margin.top + margin.bottom);
 	
 	var parseDate = d3.time.format("%d-%b-%y").parse;
 	var formatTime = d3.time.format("%e %B");
@@ -122,9 +103,12 @@ function lineChart() {
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.top + margin.bottom)
 	.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		.call(d3.behavior.zoom().on("zoom", function() {
+			svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+		}))
 	
-	d3.csv("dataset/data.csv", function(error, date) {
+	d3.csv("dataset/data.csv", function(error, data) {
 		data.forEach(function(d) {
 			d.date = parseDate(d.date);
 			d.close = +d.close;
