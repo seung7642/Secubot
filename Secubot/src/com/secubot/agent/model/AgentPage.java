d99634dd63ab344e68fa40a554bced081b26a883
SEUNGHO <seung7642@gmail.com>
2018-12-05 20:55:27 +0900

    #Agent <-> RDB 기능을 하는 MVC 파일 작업 중

--- Secubot/src/com/secubot/agent/model/AgentPage.java
+++ Secubot/src/com/secubot/agent/model/AgentPage.java
@@ -1,5 +1,26 @@
 package com.secubot.agent.model;
 
+import java.util.List;
+
 public class AgentPage {
 
+	private int total;
+	private List<LoginSession> list;
+	
+	public AgentPage(int total, List<LoginSession> list) {
+		this.total = total;
+		this.list = list;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	public boolean hasAgentList() {
+		return total > 0;
+	}
 }
