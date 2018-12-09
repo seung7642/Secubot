a13d06cb2c776051e6e277de7d79b9e553d56154
SEUNGHO <seung7642@gmail.com>
2018-12-09 18:56:13 +0900

    #Working of Report Page ...

--- Secubot/src/com/secubot/report/command/ReportHandler.java
+++ Secubot/src/com/secubot/report/command/ReportHandler.java
@@ -13,6 +13,22 @@ public class ReportHandler implements CommandHandler {
 	
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) {
+		if (req.getMethod().equalsIgnoreCase("GET")) {
+			return processForm(req, res);
+		} else if (req.getMethod().equalsIgnoreCase("POST")) {
+			return processSubmit(req, res);
+		} else {
+			res.setStatus(res.SC_METHOD_NOT_ALLOWED);
+			return null;
+		}
+	}
+	
+	private String processForm(HttpServletRequest req, HttpServletResponse res) {
+		return FORM_VIEW;
+	}
+	
+	private String processSubmit(HttpServletRequest req, HttpServletResponse res) {
 		
+		return null;
 	}
 }
